6f3d6027f724be3785f1473e8f5bf4af84dc42d0
trevor-pahigian <trevor.pahigian@deel.com>
2023-01-28 12:04:55 -0500

    upate json for icon and image

--- picker.js
+++ picker.js
@@ -1,26 +1,27 @@
-  $("#click").on('click', function(evt) {
+  $("#recipe_picker").on('click', function(evt) {
+    
+    var column = document.getElementsByClassName('column')
+    column[0].style.display = "block";
+    column[1].style.display = "block";
+
+    var title = document.getElementById('title')
+    title.style.display = 'none';
+
     // pick random recipe
     async function foo() {
       let obj;
-    
       const res = await fetch('meals.json')
-    
       obj = await res.json();
-    
       const ansIndex = Math.floor(Math.random() * (8));
-      
       
       // Display recipe 
       const recipe = obj.meals[ansIndex].recipe;
-      $("#meal_container h1").text(recipe);
-      console.log(recipe);
-
+      $("#meal_container h2").text(recipe);
+      // console.log(recipe);
 
       // Display ingredient       
-      var ingredient = obj.meals[ansIndex].ingredient;
-    
+      var ingredient = obj.meals[ansIndex].ingredient;    
       document.getElementById(`ingredients_list`).innerHTML = "";
-      
       var charArry = ingredient.split('; ');
       let ul = document.querySelector('#ingredients_list')
       charArry.forEach(liTxt => {
@@ -31,14 +32,11 @@
       ul.appendChild(li)
       li.appendChild(input)
       })
-      console.log(ingredient);
+      // console.log(ingredient);
 
-    
       // Display Instructions 
       var instruction = obj.meals[ansIndex].instruction;
-
       document.getElementById(`instructions_list`).innerHTML = "";
-    
       var charArry = instruction.split('; ');
       let instruction_ul = document.querySelector('#instructions_list')
       charArry.forEach(liTxt => {
@@ -46,19 +44,32 @@
       li.innerHTML = liTxt
       instruction_ul.appendChild(li)
       })
-      console.log(instruction);
-
+      // console.log(instruction);
 
       // Display Link 
       var link = obj.meals[ansIndex].link;
       var link_id = document.querySelector('#link')
       link_id.href = link;
       link_id.style.display = "block";
-      console.log(link);
+      // console.log(link);
+
+      var img_id = document.querySelector('#image')
+      var img = document.createElement("img");
+      img.src = obj.meals[ansIndex].image;
+      img.style.width = "inherit";
+      img.style.height = "inherit";
+      img_id.innerHTML = ""
+      img_id.appendChild(img);
+
+      var icons = ["fa-bacon", "fa-bread-slice", "fa-cheese", "fa-drumstick-bite", "fa-egg", "fa-hamburger", "fa-hotdog", "fa-ice-cream", "fa-pepper-hot", "fa-pizza-slice"]
+      const rand = Math.floor(Math.random() * (10));
+      var icon = document.querySelector("#meal_icon");
+      icon.className = ''
+      icon.classList.add("fas", icons[rand])
+      // console.log(icons[rand]);
+
     }
-    
+
     foo();
-
-
 
   });
